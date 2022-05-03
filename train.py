95560324a54e6abf17bef9e0d0f5d81f969934ed
Dominique NGA TONTSA <76219175+naicka@users.noreply.github.com>
2022-05-03 13:48:58 +0200

    Add files via upload

---
+++ train.py
@@ -0,0 +1,17 @@
+from json import loads, dumps
+from sklearn.linear_model import LinearRegression
+
+with open('input.data.json') as f:
+  content = f.read()
+  TRAIN_INPUT = loads(content)
+
+with open('output.data.json') as f:
+  content = f.read()
+  TRAIN_OUTPUT = loads(content)
+
+predictor = LinearRegression(n_jobs=-1)
+
+predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
+
+with open('model.json', 'w') as f:
+  f.write(dumps(predictor.coef_.tolist()))
